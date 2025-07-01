0096b23343a2c30d3c0cc0ef3cee2d437859df3e
anasnofal <anasnofal14@yahoo.com>
2025-07-01 16:13:33 +0200

    fix error

--- src/routes/data.py
+++ src/routes/data.py
@@ -5,6 +5,8 @@ from controllers import DataController,ProjectController
 import os
 import aiofiles
 from models import ResponseResult
+import logging
+logger = logging.getLogger('uvicorn.error')
 
 data_router = APIRouter(
     prefix="/api/v1/data",
@@ -14,7 +16,8 @@ data_router = APIRouter(
 async def upload_data(project_id: str, file: UploadFile,
                     app_settings : Settings = Depends(get_settings) ) :
     # validate the file properties
-    is_valid, result_msg= DataController().validate_uploaded_file(file=file)
+    data_controller=DataController()
+    is_valid, result_msg= data_controller.validate_uploaded_file(file=file)
 
     if not is_valid:
         return JSONResponse(
@@ -25,14 +28,21 @@ async def upload_data(project_id: str, file: UploadFile,
             }
         )
     project_dir_path = ProjectController().get_project_path(project_id=project_id)
-    file_path= os.path.join(
-        project_dir_path,
-        file.filename
-    )
-    async with aiofiles.open(file_path,"wb") as f :
-        while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
-            await f.write(chunk)
-
+    file_path= data_controller.generate_unique_filepath(orig_file_name=file.filename,
+                                                        project_id=project_id)
+    try :
+        async with aiofiles.open(file_path,"wb") as f :
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as e:
+            logger.error(f"Error while uploading file :{e}")
+            return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "result":is_valid,
+                "result_msg":ResponseResult.FILE_UPLOAD_FAILED.value
+            }
+        )
     return JSONResponse(
                 content={
                     "result":is_valid,
