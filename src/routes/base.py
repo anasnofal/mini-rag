9150ac330af42b83fcda5c3fa9e5ec8dfb51abdf
anasnofal <anasnofal14@yahoo.com>
2025-07-01 13:55:52 +0200

    .

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter
+from helpers.config import get_settings
 import os
 
 
@@ -9,8 +10,9 @@ base_router = APIRouter(
 
 @base_router.get('/')
 async def welcome() :
-    app_name = os.getenv("APP_NAME")
-    app_version = os.getenv("APP_VERSION")
+    app_settings = get_settings()
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
     return {
         "app_name": app_name,
         "app_version": app_version,
