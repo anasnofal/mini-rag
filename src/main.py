1cedc5e6552dbf42dae1e9b56547d422f3514ca2
anasnofal <anasnofal14@yahoo.com>
2025-07-26 14:50:52 +0200

    Add LLMProviderFactory and update config settings for OpenAI and Cohere integration

--- src/main.py
+++ src/main.py
@@ -2,17 +2,25 @@ from fastapi import FastAPI
 from routes import base, data
 from pymongo import AsyncMongoClient as Client
 from helpers.config import get_settings
+from stores.llm.LLMProviderFactory import LLMProviderFactory
 
 app = FastAPI()
-@app.on_event("startup")
 async def startup_dv_client():
     settings = get_settings()
     app.mongo_conn = Client(settings.MONGODB_URL)
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+    llm_provider_factory = LLMProviderFactory(settings)
 
-@app.on_event("shutdown")
+    app.generation_client = llm_provider_factory.create(settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(settings.GENERATION_MODEL_ID)
+
+    app.embedding_client = llm_provider_factory.create(settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(settings.EMBEDDING_MODEL_ID, settings.EMBEDDING_MODEL_SIZE)
+
 async def shutdown_db_client():
     app.mongo_conn.close()
 
+app.router.lifespan.on_startup.append(startup_dv_client)
+app.router.lifespan.on_shutdown.append(shutdown_db_client)  
 app.include_router(base.base_router)
 app.include_router(data.data_router)
