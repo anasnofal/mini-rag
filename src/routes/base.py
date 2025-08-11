cdb200d966caa863798c38c840038e142569852e
anasnofal <anasnofal14@yahoo.com>
2025-08-11 15:25:32 +0200

    Fix formatting and add current time to welcome response in base router

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, APIRouter, Depends
-from helpers.config import get_settings,Settings
+from helpers.config import get_settings, Settings
 import os
+from datetime import datetime
 
 
 base_router = APIRouter(
@@ -8,12 +9,13 @@ base_router = APIRouter(
     tags=["api_v1"],
 )
 
-@base_router.get('/')
-async def welcome(app_settings : Settings = Depends(get_settings)) :
+
+@base_router.get("/")
+async def welcome(app_settings: Settings = Depends(get_settings)):
     app_name = app_settings.APP_NAME
     app_version = app_settings.APP_VERSION
     return {
         "app_name": app_name,
         "app_version": app_version,
-
+        "current_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
     }
